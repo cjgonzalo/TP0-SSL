0dab063bb0586e17f1ebd082d2658beb94b2be66
cjgonzalo <gonicasanova.gc@gmail.com>
2021-09-03 16:43:55 -0300

    cambio while por do while para contemplar cadena vacía

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include "scanner.h"
 
-int main(){
+int main(void){
   int token;
-  token = get_token();
-  while(token != FDT){
-    switch(token){
-      case CAD:
-        printf("Cadena: %s\n", buffer);
-        buffer[0] = '\0';
-        break;
-      case SEP:
-        printf("Separador: %c\n", buffer[0]);
-        break;
-      case FDT:
-        printf("Fin de texto");
-        break;
-    }
-    token = get_token();
-  }
-
-  return 0;
+  do{
+      token = get_token();
+      switch(token){
+         case CAD:
+            printf("Cadena: %s\n", buffer);
+            buffer[0] = '\0';
+            break;
+         case SEP:
+            printf("Separador: %c\n", buffer[0]);
+            break;
+         case FDT:
+            printf("Fin de texto \n");
+            break;
+      }
+   }while(token != FDT);
+   return 0;
 }
--- scanner.c
+++ scanner.c
@@ -1,21 +1,20 @@
 #include "scanner.h"
 
 int get_token(){
-    int caracter = getchar();
-    if(caracter == EOF){
+   char caracter = getchar();
+   if(caracter == EOF){
       return FDT;
-    } else if(caracter == ','){
+   } else if(caracter == ','){
       buffer[0] = caracter;
       return SEP;
-    } else if(!isspace(caracter)){
+   } else if(!isspace(caracter)){
       int i;
-      for(i = 0; caracter != EOF && caracter != ',' && !isspace(caracter); i++){
-        buffer[i] = caracter;
-        caracter = getchar();
+      for(i = 0; caracter != EOF && caracter != ',' && isspace(caracter); i++){
+         buffer[i] = caracter;
+         caracter = getchar();
       }
       ungetc(caracter, stdin);
       buffer[i] = '\0';
       return CAD;
-    }
-
+   }
 }
